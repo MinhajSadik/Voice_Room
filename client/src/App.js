48572d24fa8dc13b2bfe91049fe5fe9e4bf05bf9
MinhajSadik <MinhajoSadik@gmail.com>
2022-05-10 02:41:01 +0600

    [ADD] working for protected routing system

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,83 @@
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Redirect,
+  Route,
+  Switch,
+} from "react-router-dom";
 import "./App.css";
 import Navigation from "./Components/Shared/Navigation/Navigation";
+import Activate from "./pages/Activate/Activate";
+import Authenticate from "./pages/Authenticate/Authenticate";
 import Home from "./pages/Home/Home";
-import Login from "./pages/Login/Login";
-import Register from "./pages/Register/Register";
+
+const isAuth = true;
+const user = {
+  activated: false,
+};
 
 function App() {
   return (
     <Router>
       <Navigation />
       <Switch>
-        <Route path="/" exact component={Home} />
-        <Route path="/register" exact component={Register} />
-        <Route path="/login" exact component={Login} />
+        <GuestRoute path="/" exact>
+          <Home />
+        </GuestRoute>
+        <GuestRoute path="/authenticate">
+          <Authenticate />
+        </GuestRoute>
+        <SemiProtectedRoute path="/activate">
+          <Activate />
+        </SemiProtectedRoute>
       </Switch>
     </Router>
   );
 }
 
+const GuestRoute = ({ children, ...rest }) => {
+  return (
+    <Route
+      {...rest}
+      render={({ location }) => {
+        return isAuth ? (
+          <Redirect
+            to={{
+              pathname: "/rooms",
+              state: { from: location },
+            }}
+          />
+        ) : (
+          children
+        );
+      }}
+    ></Route>
+  );
+};
+
+const SemiProtectedRoute = ({ children, ...rest }) => {
+  return (
+    <Route
+      {...rest}
+      render={({ location }) => {
+        return !isAuth ? (
+          <Redirect
+            to={{
+              pathname: "/",
+              state: { from: location },
+            }}
+          />
+        ) : isAuth && !user.activated ? (
+          children
+        ) : (
+          <Redirect
+            to={{
+              pathname: "/rooms",
+              state: { from: location },
+            }}
+          />
+        );
+      }}
+    ></Route>
+  );
+};
 export default App;
---
+++ client/src/pages/Authenticate/Authenticate.jsx
@@ -0,0 +1,23 @@
+import React, { useState } from "react";
+import StepOtp from "../Steps/StepOtp/StepOtp";
+import StepPhoneEmail from "../Steps/StepPhoneEmail/StepPhoneEmail";
+
+const steps = {
+  1: StepPhoneEmail,
+  2: StepOtp,
+};
+
+const Authenticate = () => {
+  const [step, setStep] = useState(1);
+  const Step = steps[step];
+  function onNext() {
+    setStep((prevState) => prevState + 1);
+  }
+  return (
+    <div>
+      <Step onNext={onNext} />
+    </div>
+  );
+};
+
+export default Authenticate;
