953571430dc67f539752ef47687eb741d0f858eb
MinhajSadik <MinhajoSadik@gmail.com>
2022-05-09 22:52:16 +0600

    [ADD] worked in home page for doing dynamic content load

--- client/src/Components/Shared/Card/Card.jsx
+++ client/src/Components/Shared/Card/Card.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import styles from "./Card.module.css";
 
-const Card = ({ title, icon }) => {
+const Card = ({ title, icon, children }) => {
   return (
     <div className={styles.card}>
       <div className={styles.headingWrapper}>
--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -5,7 +5,11 @@ import styles from "./Home.module.css";
 const Home = () => {
   return (
     <div className={styles.cardWrapper}>
-      <Card title="Welcome to CodersHouse Voice Room!" icon="handlogo" />
+      <Card title="Welcome to CodersHouse Voice Room!" icon="handlogo">
+        I’m working hard to get Codershouse ready for everyone! While we wrap up
+        the finishing touches, we're adding people gradually to make sure
+        nothing breaks
+      </Card>
     </div>
   );
 };
