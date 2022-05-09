335dbfddf86c3f8d62faefe3fa5d7c6ee903e8e9
MinhajSadik <MinhajoSadik@gmail.com>
2022-05-09 21:15:43 +0600

    [ADD] worked in home page for doing dynamic content load

---
+++ client/src/Components/Shared/Card/Card.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import styles from "./Card.module.css";
+
+const Card = ({ title, icon }) => {
+  return (
+    <div className={styles.card}>
+      <div className={styles.headingWrapper}>
+        <img src={`/images/${icon}.png`} alt="logo" />
+        <h1 className={styles.heading}>{title}</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import Card from "../../Components/Shared/Card/Card";
+import styles from "./Home.module.css";
 
 const Home = () => {
   return (
-    <div>
-      <header>This is our home page</header>
+    <div className={styles.cardWrapper}>
+      <Card title="Welcome to CodersHouse Voice Room!" icon="handlogo" />
     </div>
   );
 };
