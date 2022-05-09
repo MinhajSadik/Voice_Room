84e7287ca215c06b4c56a6279ec0e4c06eab558b
MinhajSadik <MinhajoSadik@gmail.com>
2022-05-09 17:26:01 +0600

    [ADD] working in voice_apps for basic view

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,16 @@
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
+import Navigation from "./Components/Shared/Navigation/Navigation";
+import Home from "./pages/Home/Home";
 
 function App() {
   return (
-    <div>
-      <header>Welcome to Coders House</header>
-    </div>
+    <Router>
+      <Navigation />
+      <Switch>
+        <Route path="/" exact component={Home} />
+      </Switch>
+    </Router>
   );
 }
 
---
+++ client/src/Components/Shared/Navigation/Navigation.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Navigation = () => {
+  return (
+    <div>This is Navigation</div>
+  )
+}
+
+export default Navigation
