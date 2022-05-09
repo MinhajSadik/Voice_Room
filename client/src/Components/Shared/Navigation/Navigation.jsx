48da2c0cb860d53513c6addee41c8e2cc0ed485d
MinhajSadik <MinhajoSadik@gmail.com>
2022-05-09 20:15:43 +0600

    [ADD] voice room navbar and home page worked something done

--- client/src/Components/Shared/Navigation/Navigation.jsx
+++ client/src/Components/Shared/Navigation/Navigation.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+import styles from "./Navigation.module.css";
 
 const Navigation = () => {
-  return (
-    <div>This is Navigation</div>
-  )
-}
+  const brandStyle = {
+    color: "#fff",
+    textDecoration: "none",
+    fontSize: "1.5rem",
+    fontWeight: "bold",
+    display: "flex",
+    alignItems: "center",
+  };
+  const logoText = {
+    marginLeft: "0.5rem",
+  };
 
-export default Navigation
+  return (
+    <nav className={`${styles.navbar} container`}>
+      <Link style={brandStyle} to="/">
+        <img src="/images/logo.png" alt="logo" />
+        <span style={logoText}>Voice Room</span>
+      </Link>
+    </nav>
+  );
+};
+
+export default Navigation;
