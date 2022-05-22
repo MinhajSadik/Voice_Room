18a7ba0503f24e8bf347dd976981c991820113e2
MinhajSadik <MinhajoSadik@gmail.com>
2022-05-22 23:55:26 +0600

    [ADD] working for rooms creating

--- server/database.js
+++ server/database.js
@@ -6,7 +6,7 @@ function DbConnect() {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   };
-  const DB_URL = process.env.DB_URL_COMPUSER;
+  const DB_URL = process.env.DB_URL_COMPASS;
   mongoose.connect(DB_URL, options);
   const db = mongoose.connection;
   db.on("error", console.error.bind(console, "connection error:"));
