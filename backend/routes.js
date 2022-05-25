0a0e121b131f461814bdc941ed107ed8e842f37e
MinhajSadik <MinhajoSadik@gmail.com>
2022-05-25 18:01:30 +0600

    [ADD] nothing to do

--- backend/routes.js
+++ backend/routes.js
@@ -6,7 +6,7 @@ const authMiddlware = require("./middlewares/auth-middlware");
 router.post("/api/send-otp", authController.sendOtp);
 router.post("/api/verify-otp", authController.verifyOtp);
 router.post("/api/activate", authMiddlware, activateController.activate);
-// router.get("/api/refresh", authController.refresh);
+router.get("/api/refresh", authController.refresh);
 router.post("/api/logout", authMiddlware, authController.logout);
 
 module.exports = router;
